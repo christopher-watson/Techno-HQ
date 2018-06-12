6f119f00787aaf71a80f70ff8eaae19958bb7ed3
Christopher Watson <christopher_watson@icloud.com>
2018-06-12 13:55:42 -0400

    styling

--- public/app.js
+++ public/app.js
@@ -1,42 +1,46 @@
 // Grab the articles as a json
-$.getJSON("/events", function(data) {
+$.getJSON("/events", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#events").append("<h3 data-id='" + data[i]._id + "'>" + data[i].title + "</h3>");
-    if(data[i].img[0] === '/'){
-      $("#events").append('<img src="https://www.residentadvisor.net' + data[i].img + '">');
+    var eventDiv = $("<div>");
+    var eventID = data[i]._id;
+    eventDiv.append("<h3 class='event-title' data-id='" + data[i]._id + "'>" + data[i].title + "</h3>");
+    if (data[i].img[0] === '/') {
+      eventDiv.append('<img class="event-pic" src="https://www.residentadvisor.net' + data[i].img + '">');
+    } else {
+      eventDiv.append('<img class="event-pic" src="https://pbs.twimg.com/profile_images/876409189379297280/-zjcSD8t_200x200.jpg">');
     }
-    else{
-      $("#events").append('<img src="https://pbs.twimg.com/profile_images/876409189379297280/-zjcSD8t_200x200.jpg">');
-    }
+    eventDiv.append('<div class="notes" data-id="' + data[i]._id + '"><div>');
+    eventDiv.addClass('inner-event');
+    eventDiv.attr('id', eventID);
+    $("#events").append(eventDiv);
   }
 });
 
 
-// Whenever someone clicks a p tag
-$(document).on("click", "h3", function() {
+//  WORKING 
+$(document).on("click", "h3", function () {
   // Empty the notes from the note section
-  $("#notes").empty();
+  $(".notes").empty();
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
   // Now make an ajax call for the Article
   $.ajax({
-    method: "GET",
-    url: "/events/" + thisId
-  })
+      method: "GET",
+      url: "/events/" + thisId
+    })
     // With that done, add the note information to the page
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
+      $(".notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      $(".notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $(".notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $(".notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
       // If there's a note in the article
       if (data.note) {
@@ -48,28 +52,29 @@ $(document).on("click", "h3", function() {
     });
 });
 
+
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#savenote", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
-    method: "POST",
-    url: "/events/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
-  })
+      method: "POST",
+      url: "/events/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
     // With that done
-    .then(function(data) {
+    .then(function (data) {
       // Log the response
       console.log(data);
       // Empty the notes section
-      $("#notes").empty();
+      $(".notes").empty();
     });
 
   // Also, remove the values entered in the input and textarea for note entry
